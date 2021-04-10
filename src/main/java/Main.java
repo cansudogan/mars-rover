3b65bee171adb05d4dfd00ddbe002077da118bdb
cansudogan <cansudogan95@gmail.com>
2021-04-10 17:03:59 +0300

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
+import direction.implementation.East;
 import plateau.Plateau;
 import rover.MarsRover;
 
 public class Main {
-    public static void main(String[] args){
-        Plateau plateau = new Plateau(3,3);
-
+    public static void main(String[] args) {
+        Plateau plateau = new Plateau(5, 5);
+        MarsRover marsRover = new MarsRover(plateau);
+        East east = new East();
+        marsRover.setCoordinateX(1);
+        marsRover.setCoordinateY(0);
+        marsRover.setDirection(east);
+        System.out.println(marsRover.broadcast());
+        marsRover.turnLeft();
+        marsRover.move();
+        System.out.println(marsRover.broadcast());
     }
 }
